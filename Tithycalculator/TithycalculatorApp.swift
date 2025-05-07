739bc262cab8588ada2b463cb0e7f34f29493fa9
Irvens Dupuy <irvensdupuy@yahoo.com>
2025-05-07 17:00:56 -0500

    Initial Commit

---
+++ Tithycalculator/TithycalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TithycalculatorApp.swift
+//  Tithycalculator
+//
+//  Created by Irvens Dupuy on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TithycalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
