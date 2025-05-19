9e547a8de05eb0d89dac8acda50edc490227541e
Irvens Dupuy <irvensdupuy@yahoo.com>
2025-05-19 17:06:58 -0500

    Initial Commit

---
+++ Tithycalculator/TithycalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TithycalculatorApp.swift
+//  Tithycalculator
+//
+//  Created by Irvens Dupuy on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TithycalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TithycalculatorTests/TithycalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TithycalculatorTests.swift
+//  TithycalculatorTests
+//
+//  Created by Irvens Dupuy on 5/19/25.
+//
+
+import Testing
+@testable import Tithycalculator
+
+struct TithycalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
